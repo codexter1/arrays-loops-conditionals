4bad7c2ef9bdd90ae3378a4548db837ad003f67c
Tommy Arbuthnot <tommyarbuthnot@yahoo.COM>
2018-03-21 22:17:19 -0500

    excited kittens

--- answers.js
+++ answers.js
@@ -7,3 +7,12 @@ for (i = 1; i <=200; i++) {
       console.log(i);
   }
 };
+let phrase = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+for (var i = 1; i < 20 ; i++) {
+  if(i % 2 === 0) {
+    let a = Math.floor(Math.random()*(phrase.length));
+    console.log(phrase[a]);
+  } else {
+    console.log("Love me, pet me! HSSSSSS!");
+  }
+};
