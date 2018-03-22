bc4bcbd3840b35ba235233d2d2654ab8cc2c47b8
Tommy Arbuthnot <tommyarbuthnot@yahoo.COM>
2018-03-21 22:36:11 -0500

    fizzbuzz

--- answers.js
+++ answers.js
@@ -1,12 +1,13 @@
 for (i = 1; i <=20; i++) {
   console.log(i);
 };
-
+//evens
 for (i = 1; i <=200; i++) {
   if (i % 2 === 0) {
       console.log(i);
   }
 };
+//kittens
 let phrase = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
 for (var i = 1; i < 20 ; i++) {
   if(i % 2 === 0) {
@@ -16,3 +17,15 @@ for (var i = 1; i < 20 ; i++) {
     console.log("Love me, pet me! HSSSSSS!");
   }
 };
+//fizzbuzz
+for (i = 1; i < 101; i++) {
+  if (i % 15 === 0) {
+    console.log('fizzbuzz');
+  } else if (i % 3 === 0) {
+    console.log('fizz');
+  } else if (i % 5 === 0) {
+    console.log('buzz');
+  } else {
+    console.log(i);
+  }
+}
